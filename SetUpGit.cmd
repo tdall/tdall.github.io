99a1e5c26f07125cb8f983c61c2ffa727afea472
Tammy Dallman <tammy_dallman@hotmail.com>
2018-06-14 15:48:34 -0700

    Update SetUpGit.cmd

--- SetUpGit.cmd
+++ SetUpGit.cmd
@@ -2,7 +2,7 @@ echo "# Doing my initial setup " >> README.md
 git init
 git add .
 git commit -m "first commit"
-git remote add origin https://github.com/tdall/chicktech.git
+git remote add origin https://github.com/tdall/tdall.github.io.git
 git push -u origin master
 PAUSE
 
